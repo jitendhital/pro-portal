bb2758dddd438c576267b1edfa32c7c07b38f787
jitendhital <jitendhital70@gmail.com>
2025-08-19 21:58:15 +0545

    created a test api routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://jiten:jiten@pro-portal.r9ldr7q.mongodb.net/?retryWrites=true&w=majority&appName=pro-portal')
@@ -9,3 +10,5 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 }
 )
+
+app.use('/api/user', userRouter);
