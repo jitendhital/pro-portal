34f34cf33e83518dc50644c9eb0be6179ec92afd
jitendhital <jitendhital70@gmail.com>
2025-08-19 13:25:35 +0545

    creates routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-
-import React from 'react'
+import {BrowserRouter , Routes, Route, } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/"  element={<Home />} />
+      <Route path="/signIn"element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile"element={<Profile />} />
+      
+    </Routes>
+  </BrowserRouter>;
 }
