764eac4cba77cad7a61ab8a127ec5df7b70cc61d
jitendhital <jitendhital70@gmail.com>
2025-08-22 12:42:37 +0545

    created sighup api routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://jiten:jiten@pro-portal.r9ldr7q.mongodb.net/?retryWrites=true&w=majority&appName=pro-portal')
 const app = express();
+
+app.use(express.json()); // Middleware to parse JSON bodies
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
@@ -12,3 +15,4 @@ app.listen(3000,()=>{
 )
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
